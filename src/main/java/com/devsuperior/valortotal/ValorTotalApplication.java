563a468b232c66013706503c1adf85418c1e1a86
Vitor-PereiraS <vitorpereirasou2010@gmail.com>
2025-08-29 13:32:12 -0300

    Initial commit

---
+++ src/main/java/com/devsuperior/valortotal/ValorTotalApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.valortotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValorTotalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValorTotalApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/devsuperior/valortotal/entities/Order.java
@@ -0,0 +1,4 @@
+package com.devsuperior.valortotal.entities;
+
+public class Order {
+}
